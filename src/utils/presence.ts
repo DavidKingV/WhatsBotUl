43968537c579dbadf0df0858ef5e454ce725c693
David Vega <qet135ui@hotmail.com>
2024-10-07 21:41:06 -0600

    Se corrigen funciones

--- src/utils/presence.ts
+++ src/utils/presence.ts
@@ -1,16 +1,3 @@
-const typing = async function (ctx: any, provider: any) {
-    if (provider && provider?.vendor && provider.vendor?.sendPresenceUpdate) {
-        const id = ctx.key.remoteJid
-        await provider.vendor.sendPresenceUpdate('composing', id)
-    }
-}
-const recording = async function (ctx: any, provider: any) {
-    if (provider && provider?.vendor && provider.vendor?.sendPresenceUpdate) {
-        const id = ctx.key.remoteJid
-        await provider.vendor.sendPresenceUpdate('recording', id)
-    }
-}
-
 const numberClean = (raw: string): string => {
     //Mute +3400000 
     const number = raw.toLowerCase().replace('mute', '').replace(/\s/g, '').replace('+', '')
@@ -18,4 +5,4 @@ const numberClean = (raw: string): string => {
     return number
 }
 
-export { typing, recording, numberClean }
+export { numberClean }
